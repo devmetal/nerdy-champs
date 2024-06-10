32b6eb08194ca6d8918b78c2875f765cda913af7
devmetal <devmetal91@gmail.com>
2024-06-10 14:44:16 +0200

    create event cards

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class GamesController < ApplicationController # rubocop:disable Style/Documentation
-  def index; end
+  def index
+    @events = Event.order(created_at: :desc).limit(4)
+  end
 end
